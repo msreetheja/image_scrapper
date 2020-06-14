22311b31a555b6cbdeb87c6f6aa81f25060777cd
Sree Theja <msreetheja@gmail.com>
2020-06-15 00:58:26 +0530

    added image extension when saving down to disk

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use infer;
 use reqwest;
 use scraper::{Html, Selector};
 use std::fs::File;
@@ -10,13 +11,24 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     .text()?;
     let document = Html::parse_document(&resp);
     let img_selector = Selector::parse("img").unwrap();
-    let mut i = 0;
     for img_node in document.select(&img_selector) {
-        i += 1;
         if let Some(url) = img_node.value().attr("src") {
             println!("downloading {}", url);
             let img_bytes = reqwest::blocking::get(url)?.bytes()?;
-            let mut file = File::create(format!("downloads/pic_{}.png", i)).unwrap();
+            let img_extension = infer::Infer::new()
+                .get(&img_bytes)
+                .unwrap_or(infer::Type {
+                    mime: String::from("image/png"),
+                    ext: String::from("png"),
+                })
+                .ext;
+            let pic_name = url
+                .split("/")
+                .last()
+                .and_then(|x| x.split(".").next())
+                .unwrap_or("nameless");
+            let mut file =
+                File::create(format!("downloads/pic_{}.{}", pic_name, img_extension)).unwrap();
             io::copy(&mut img_bytes.as_ref(), &mut file).unwrap();
             println!("dowloaded {}", url);
         }
